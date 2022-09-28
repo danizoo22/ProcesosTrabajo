5aa68c09d52f3fb0249f14f73dd3e69169d2b68d
danizoo22 <110929998+danizoo22@users.noreply.github.com>
2022-09-28 10:40:47 +0200

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+package Probando;
+
+public class HolaMundo {
+	public static void main(String [] args) {
+		System.out.println("Hola mundo");
+		System.out.println("Adios mundo");
+		int x = 5;
+		System.out.println("Hola mundo "+x);
+	}
+}
